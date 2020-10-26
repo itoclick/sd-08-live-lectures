80575a2c5d7b544cf59445ac6db037ca034a660a
Gustavo Caetano <gustavo.caetano@betrybe.com>
2020-10-26 17:00:54 -0300

    gera 6 números aleatorios

---
+++ megasena.js
@@ -0,0 +1,13 @@
+//array vazio com os números sortedos
+
+
+let firstNumber = Math.ceil(Math.random() * 60);
+let secondNumber = Math.ceil(Math.random() * 60);
+let thirdNumber = Math.ceil(Math.random() * 60);
+let fourthNumber = Math.ceil(Math.random() * 60);
+let fifthNumber = Math.ceil(Math.random() * 60);
+let sixthNumber = Math.ceil(Math.random() * 60);
+
+let megaSenaNumbers = [firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber, sixthNumber];
+
+console.log(megaSenaNumbers);
