ebc6625c32e3bb40278fcc567c797b0592328fe0
Gustavo Caetano <gustavo.caetano@betrybe.com>
2020-10-27 17:34:08 -0300

    verifica vertical, horizontal e superior direito

---
+++ queen-attack.js
@@ -0,0 +1,31 @@
+//posicao da rainha
+let posicaoRainhaLinha = 4;
+let posicaoRainhaColuna = 4;
+
+//posicao da outra peca
+let posicaoPecaLinha = 7;
+let posicaoPecaColuna = 7;
+
+//identifica se o ataque pode ocorrer ou não
+let ataqueBemSucedido = false;
+
+if (posicaoRainhaLinha === posicaoPecaLinha || posicaoRainhaColuna === posicaoPecaColuna) {
+  ataqueBemSucedido = true;
+}
+
+//diagonal superior direita
+for (let supDir = 1; supDir < 8; supDir += 1) {
+  let linhaRainha = posicaoRainhaLinha + supDir;
+  let colunaRainha = posicaoRainhaColuna + supDir;
+
+   //se o tabuleiro acabou, sai do loop
+  if (linhaRainha > 8 || colunaRainha > 8) {
+    break;
+  }
+
+  if ((posicaoPecaLinha == linhaRainha) && (posicaoPecaColuna == colunaRainha)) {
+    ataqueBemSucedido = true;
+  }
+}
+
+console.log(ataqueBemSucedido);
