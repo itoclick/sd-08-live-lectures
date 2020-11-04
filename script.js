a930524180e9203d2f6674cba117e731e8678547
Gustavo Caetano <gustavo.caetano@betrybe.com>
2020-11-04 18:07:10 -0300

    funcao do carro que vai até o final da tela

--- script.js
+++ script.js
@@ -12,7 +12,21 @@ window.onload = function () {
     
     car1.style.marginLeft = parseInt(car1.style.marginLeft) + (Math.random() * 100) + 'px';
     car2.style.marginLeft = parseInt(car2.style.marginLeft) + (Math.random() * 100) + 'px';
+
+    if (parseInt(car1.style.marginLeft) > window.innerWidth) {
+      alert('Carro VERMELHO ganhou!');
+      resetCars(car1, car2);
+    };
+
+    if (parseInt(car2.style.marginLeft) > window.innerWidth) {
+      alert('Carro VERDE ganhou!');
+      resetCars(car1, car2);
+    };
+
   });
 };
 
-
+function resetCars (car1, car2) {
+  car1.style.marginLeft = '0px';
+  car2.style.marginLeft = '0px';
+};
