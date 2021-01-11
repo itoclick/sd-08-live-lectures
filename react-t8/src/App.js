b2c28b67fc3fc231bc16711811597827fa9fa7e2
Ícaro Harry <me@icaro.io>
2021-01-11 19:06:12 -0300

    :sparkles: Aula 11.1 - Introdução à React

---
+++ react-t8/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Footer from './components/Footer.js';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        Olá turma 8...
+        <Footer />
+      </div>
+    )
+  }
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       Olá turma 8!
+//     </div>
+//   );
+// }
+
+export default App;
