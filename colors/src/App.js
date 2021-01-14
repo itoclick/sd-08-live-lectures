5d0bb9f2f22d07b297638dd5def94118142ede2b
Ícaro Harry <me@icaro.io>
2021-01-14 18:10:20 -0300

    Finish lecture 12.1

--- colors/src/App.js
+++ colors/src/App.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import colors from './data';
+
 import './App.css';
 
+
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      inputTextValue: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ inputTextValue: event.target.value });
+  }
+
   render() {
+    const { inputTextValue } = this.state;
     return (
-      <div>Olá T8!</div>
+      <main>
+        <input type="text" onChange={this.handleChange} />
+        <ul>
+          {
+            colors
+              .filter((color) => color.color.includes(inputTextValue))
+              .map((color) =>
+                <li key={color.value}>
+                  <div style={{ background: color.value }} className="color-square" />
+                  {color.color}
+                </li>
+              )
+          }
+        </ul>
+      </main>
     )
   }
 }
