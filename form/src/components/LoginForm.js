a3bcb09172103c7c40d95c2cc7360cd03443242a
Ícaro Harry <me@icaro.io>
2021-01-15 16:50:44 -0300

    Lecture 12-2

---
+++ form/src/components/LoginForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class LoginForm extends React.Component {
+  render() {
+    return (
+      <section className="login-form">
+        <h1>Login</h1>
+      </section>
+    );
+  }
+}
+
+export default LoginForm;
