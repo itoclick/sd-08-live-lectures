24cc8c353063e1858cb8a376ea0639ba1411a149
Ícaro Harry <me@icaro.io>
2021-01-20 18:16:27 -0300

    Finish lecture

--- clock/src/App.js
+++ clock/src/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import Clock from './components/Clock';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showDateTime: true,
+    };
+
+    this.turnOffButton = this.turnOffButton.bind(this);
+  }
+
+  turnOffButton() {
+    const { showDateTime } = this.state;
+
+    this.setState({
+      showDateTime: !showDateTime,
+    });
+  }
+
   render() {
+    const { showDateTime } = this.state;
+
     return (
       <main className="App">
+        {showDateTime
+          && <Clock
+            turnOffButton={ this.turnOffButton }
+            showDateTime={ showDateTime }
+          />}
       </main>
     );
   }
