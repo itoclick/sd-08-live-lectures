16a4904502c7d681df89abb6589e2fe914e11ed8
Ícaro Harry <me@icaro.io>
2021-02-12 15:58:50 -0300

    Init lecture

---
+++ infinityflix/src/components/Player.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class Player extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <div>
+        <h1>
+          {movie.title}
+        </h1>
+        <h3>
+          {/* {categories.name} */}
+          {' '}
+          -
+          {' '}
+          {movie.released}
+        </h3>
+        <section>
+          <iframe
+            title="trailer"
+            width="420"
+            height="315"
+            src={ movie.link }
+          />
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Player;
